ce6bef3f859c50e0e5485088fd30af73d73ca326
PUJA KUMARI <pujasingh3094@gmail.com>
2019-03-26 02:30:35 +0530

    Updated-v2-Puja

--- BookMyTime/src/main/java/com/book/mytime/repository/SlotRepository.java
+++ BookMyTime/src/main/java/com/book/mytime/repository/SlotRepository.java
@@ -1,9 +1,13 @@
 package com.book.mytime.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.book.mytime.domain.Slot;
 
 @Repository
 public interface SlotRepository extends JpaRepository<Slot,String>{
-
+	
+	List<Slot> findAllByAdminEmail(String adminEmail);
+	Boolean existsByAdminEmail(String adminEmail);
 }
